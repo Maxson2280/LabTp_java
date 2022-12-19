01a6227ed3ee7a3c09de38aa966514923cc2d910
Maxon <makim.sheshegov@gmail.com>
2022-12-20 00:48:38 +0300

    Welcome

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,41 @@
 package org.example;
 
+import java.util.concurrent.*;
+
 public class Main {
+    public static final int CARS_COUNT = 4;
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(CARS_COUNT);
+    public static CyclicBarrier startBarrier = new CyclicBarrier(CARS_COUNT + 1,
+            ()-> System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!"));
+    public static CountDownLatch finishLatch = new CountDownLatch(CARS_COUNT);
+    public static int finishCount = 0;
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        for (Car car : cars) {
+            executorService.execute(car);
+        }
+
+        try {
+            startBarrier.await();
+        } catch (InterruptedException | BrokenBarrierException ex) {
+            ex.printStackTrace();
+        }
+
+        try {
+            finishLatch.await();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        } finally {
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+            executorService.shutdown();
+        }
+
     }
 }
